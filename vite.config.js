e67dcee97dd859259def8de61f29f89079538308
OP-LT-0395 <op-lt-0395@OP-LT-0395.local>
2026-02-23 23:31:55 +0700

    security: hide secret env file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thanhsmap/', // This ensures assets load from the sub-folder
+  base: '/thanhsmap/', // This adds /thanhsmap/ to your file paths
 })
